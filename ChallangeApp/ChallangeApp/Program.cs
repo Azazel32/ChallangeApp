aa546787a94b0d83a4cd0e42223d51bef2e7726b
Azazel32 <Dawidciecko2@gmail.com>
2023-06-28 19:07:23 +0200

    else ify

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+string name;
+int age;
+string sexString;
+bool sex;
+Console.WriteLine("Podaj Imie :");
+name = Console.ReadLine();
+Console.WriteLine("Podaj wiek :");
+age = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj płeć Woman/Men:");
+sexString = Console.ReadLine();
+if (sexString=="Woman")
+{
+    sex = true;
+}
+else
+{
+    sex = false;
+}
+if (age < 30 && sex==true)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (age == 33 && name=="Ewa")
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if(age<18 && sex==false )
+{
+    Console.WriteLine("Niepolenoletni meszczyzna");
+}
