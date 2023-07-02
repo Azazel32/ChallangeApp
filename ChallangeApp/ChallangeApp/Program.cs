a2e66143e75e203fc9835c87be19b912884deed6
Azazel32 <Dawidciecko2@gmail.com>
2023-07-02 16:46:31 +0200

    gotowy program zadanie dzien 5

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,31 +1,70 @@
-﻿using System;
-string name;
-int age;
-string sexString;
-bool sex;
-Console.WriteLine("Podaj Imie :");
-name = Console.ReadLine();
-Console.WriteLine("Podaj wiek :");
-age = int.Parse(Console.ReadLine());
-Console.WriteLine("Podaj płeć Woman/Men:");
-sexString = Console.ReadLine();
-if (sexString=="Woman")
+﻿int number = 0;
+Console.WriteLine("Podaj liczbe calkowita: ");
+number = int.Parse(Console.ReadLine());
+string numberInString = number.ToString();
+char[] chars = numberInString.ToArray();
+int number_0 = 0;
+int number_1 = 0;
+int number_2 = 0;
+int number_3 = 0;
+int number_4 = 0;
+int number_5 = 0;
+int number_6 = 0;
+int number_7 = 0;
+int number_8 = 0;
+int number_9 = 0;
+foreach (char letter in chars)
 {
-    sex = true;
+    if (letter == '0')
+    {
+        number_0++;
+    }
+    else if (letter == '1')
+    {
+        number_1++;
+    }
+    else if (letter == '2')
+    {
+        number_2++;
+    }
+    else if (letter == '3')
+    {
+        number_3++;
+    }
+    else if (letter == '4')
+    {
+        number_4++;
+    }
+    else if (letter == '5')
+    {
+        number_5++;
+    }
+    else if (letter == '6')
+    {
+        number_6++;
+    }
+    else if (letter == '7')
+    {
+        number_7++;
+    }
+    else if (letter == '8')
+    {
+        number_8++;
+    }
+    else if (letter == '9')
+    {
+        number_9++;
+    }
+
+
 }
-else
-{
-    sex = false;
-}
-if (age < 30 && sex==true)
-{
-    Console.WriteLine("Kobieta ponizej 30 lat");
-}
-else if (age == 33 && name=="Ewa")
-{
-    Console.WriteLine("Ewa lat 33");
-}
-else if(age<18 && sex==false )
-{
-    Console.WriteLine("Niepolenoletni meszczyzna");
-}
+Console.WriteLine("0-->" + number_0);
+Console.WriteLine("1-->" + number_1);
+Console.WriteLine("2-->" + number_2);
+Console.WriteLine("3-->" + number_3);
+Console.WriteLine("4-->" + number_4);
+Console.WriteLine("5-->" + number_5);
+Console.WriteLine("6-->" + number_6);
+Console.WriteLine("7-->" + number_7);
+Console.WriteLine("8-->" + number_8);
+Console.WriteLine("9-->" + number_9);
