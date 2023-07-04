7f4940f2b6354f622cf2f017c3e9a734af41d4be
Azazel32 <Dawidciecko2@gmail.com>
2023-07-04 21:17:45 +0200

    Dzien8 gotowy

---
+++ ChallangeApp/ChallangeApp.Tests/ReferencesTypeTest.cs
@@ -0,0 +1,27 @@
+﻿namespace ChallangeApp.Tests
+{
+    public class ReferencesTypeTest
+    {
+        [Test]
+        public void WhenEmployeeAreNotTheSame_SchouldReturnCorect()
+        {
+            var Emp1 = AddEmployee("Adam", "Nowak", "20");
+            var Emp2 = AddEmployee("Adam", "Nowak", "20");
+
+            Assert.AreNotEqual(Emp1, Emp2);
+        }
+        [Test]
+        public void WhenEmployeeNmaeAreTheSame_SchouldReturnCorect()
+        {
+            var Emp1 = AddEmployee("Adam", "Nowak", "20");
+            var Emp2 = AddEmployee("Adam", "Nowak", "20");
+
+            Assert.AreSame(Emp1.Name, Emp2.Name);
+        }
+        private Employee AddEmployee(string name,string lastname,string age)
+        {
+            return new Employee(name, lastname, age);
+        }
+    }
+    
+}
---
+++ ChallangeApp/ChallangeApp.Tests/ValueTypeTest.cs
@@ -0,0 +1,21 @@
+﻿namespace ChallangeApp.Tests
+{
+    public class ValueTypeTest
+    {
+        [Test]
+        public void WhenNumbersValueAreTheSame_SchouldReturnCorrect()
+        {
+            int num1 = 12;
+            int num2 = 12;
+            Assert.AreEqual(num1, num2);
+        }
+        [Test]
+        public void WhenNumbersSumIsBiggerThan12_SchouldReturnCorrect()
+        {
+            int num1 = 12;
+            int num2 = 15;
+            
+            Assert.Greater(num1 + num2, 12);
+        }
+    }
+}
